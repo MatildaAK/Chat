6aeee63add840a1d54ea47803a064045eaf48d1d
MatildaAK <matilda.kallstrom@hotmail.se>
2025-11-19 12:33:00 +0100

    removed users from path

--- lib/newchat_web/live/user_forgot_password_live.ex
+++ lib/newchat_web/live/user_forgot_password_live.ex
@@ -20,8 +20,8 @@ defmodule NewchatWeb.UserForgotPasswordLive do
         </:actions>
       </.simple_form>
       <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
+        <.link href={~p"/register"}>Register</.link>
+        | <.link href={~p"/log_in"}>Log in</.link>
       </p>
     </div>
     """
--- lib/newchat_web/router.ex
+++ lib/newchat_web/router.ex
@@ -17,34 +17,6 @@ defmodule NewchatWeb.Router do
     plug :accepts, ["json"]
   end
 
-  # scope "/", NewchatWeb do
-  #   pipe_through :browser
-
-  #   get "/", PageController, :home
-  # end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", NewchatWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:newchat, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: NewchatWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
-
   ## Authentication routes
 
   scope "/", NewchatWeb do
@@ -52,13 +24,25 @@ defmodule NewchatWeb.Router do
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{NewchatWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
+      live "/register", UserRegistrationLive, :new
+      live "/log_in", UserLoginLive, :new
       live "/users/reset_password", UserForgotPasswordLive, :new
       live "/users/reset_password/:token", UserResetPasswordLive, :edit
     end
 
-    post "/users/log_in", UserSessionController, :create
+    post "/log_in", UserSessionController, :create
+  end
+
+  scope "/", NewchatWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{NewchatWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
   end
 
   scope "/", NewchatWeb do
@@ -74,15 +58,20 @@ defmodule NewchatWeb.Router do
     end
   end
 
-  scope "/", NewchatWeb do
-    pipe_through [:browser]
+    # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:newchat, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
 
-    delete "/users/log_out", UserSessionController, :delete
+    scope "/dev" do
+      pipe_through :browser
 
-    live_session :current_user,
-      on_mount: [{NewchatWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
+      live_dashboard "/dashboard", metrics: NewchatWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
