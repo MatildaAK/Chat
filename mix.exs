31837e16832bdfa79cdd15c45b5a9316120ca8f2
MatildaAK <matilda.kallstrom@hotmail.se>
2025-11-24 14:30:36 +0100

    removed bbmustache

--- mix.exs
+++ mix.exs
@@ -60,7 +60,6 @@ defmodule Newchat.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:timex, "~> 3.7.11"},
-      {:bbmustache, "~> 1.12"},
       {:pdf_generator, ">=0.5.5"}
     ]
   end
